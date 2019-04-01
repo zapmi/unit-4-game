4f55df22a2031f8367fed82e22d45efdc9d44990
zapmi <mzapata2991@gmail.com>
2019-03-31 21:47:56 -0500

    random number generated on refreshed in game added

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+$(function() {
+
+var randomNumber = "";
+
+
+document.getElementById("numberBox").innerHTML =
+Math.floor(Math.random() * 100) + 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
